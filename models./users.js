0fe42c89250aa9e5909489c3095b7d56a0273ebd
EvanORiordan <46937193+EvanORiordan@users.noreply.github.com>
2020-01-23 15:21:33 +0000

    Update users.js

--- models./users.js
+++ models./users.js
@@ -1,1 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+require('./util');
 
+var usersSchema = new Schema({
+  user_name: {type : String},
+  password: String,
+  access_token: String});
+
+//Hash password for storage in DB
+usersSchema.methods.generateHash = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+//Compare passwords to determine if user is who they say they are
+usersSchema.methods.validPassword = function(password){
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', usersSchema);
