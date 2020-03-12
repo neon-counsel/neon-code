f58dead423b63154834ab30fe09e875c50ea6f86
EvanORiordan <46937193+EvanORiordan@users.noreply.github.com>
2020-03-12 16:01:21 +0100

    add ajax to receive inputs from create project

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -7,5 +7,32 @@ $(document).ready(function () {
         window.location = "/users/register";
 
     });
+
+    $("#createProject").click(function () {
+        console.log($('input[name="privacy"]:checked').val());
+        $.ajax({
+            type: 'POST',
+            url: '/projects/new',
+            dataType: 'json',
+            data: {
+                'project_name': $("#text").val(),
+                'description': $("#desc").val(),
+                'privacy': $('input[name="privacy"]:checked').val()
+            },
+            success: function(token) {
+                $(location).attr('href', '/');
+                // Redirect to a login page
+            },
+            error: function(errMsg) {
+                swal(
+                    'Oops...',
+                    errMsg.responseJSON.body,
+                    'error'
+                )
+            }
+        });
+    });
+
+
 });
 
