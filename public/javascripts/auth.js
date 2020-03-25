682b1f53c5b6f862f192bda8b3d248c2f71b548f
adnananwar1234 <59966499+adnananwar1234@users.noreply.github.com>
2020-03-25 17:51:46 +0100

    add password strength

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -3,17 +3,13 @@ $(document).ready(
         /**
          * Event handler for when the user attempts to register
          */
+         var isFail = false;
         $("#reg-form").submit(function(event) {
             event.preventDefault();
 
-        var isFail = false;
 
 
-                  if(!$(this).val() ) {
-                        swal('Please fill all fields!');
-                        isFail = true;
 
-                  }
                   var pass = $('#inputPassword').val();
                   var pass2 = $('#confirmPassword').val();
 
@@ -72,6 +68,29 @@ $(document).ready(
                     )
                 }
             });
+        });
+
+        $('#inputPassword').keyup(function(e) {
+             var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+             var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+             var enoughRegex = new RegExp("(?=.{9,}).*", "g");
+             if (false == enoughRegex.test($(this).val())) {
+                     $('#passstrength').html('Password Should Contain 9 characters');
+                     isFail = true;
+             } else if (strongRegex.test($(this).val())) {
+                     $('#passstrength').className = 'ok';
+                     $('#passstrength').html('Strong Password!');
+                     isFail = false;
+
+             } else if (mediumRegex.test($(this).val())) {
+                     $('#passstrength').className = 'alert';
+                     $('#passstrength').html('Password Should contain <br/> Uppercase, Lowercase, number and symbol!');
+                     isFail = true;
+             } else {
+                     $('#passstrength').className = 'error';
+                     $('#passstrength').html('Password Should contain <br/> Uppercase, Lowercase, number and symbol!');
+                     isFail = true;
+             }
         });
 
     });
